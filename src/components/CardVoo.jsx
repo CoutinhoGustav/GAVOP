47542f3de43531af5ba90d834a07adcea81771b7
Gustavo Coutinho <gustavocoutinho32@gmail.com>
2025-08-03 11:28:50 -0300

    css atualizado

--- src/components/CardVoo.jsx
+++ src/components/CardVoo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./CardVoo.css";
+import "../css/CardVoo.css";
 
 
 function CardVoo({ voo }) {
